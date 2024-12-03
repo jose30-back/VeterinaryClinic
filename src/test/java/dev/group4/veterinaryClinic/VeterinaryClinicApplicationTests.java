18db2641fa6c8134c311252b56e87e45cbc77eaa
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-12-03 15:16:52 +0100

    initial project

---
+++ src/test/java/dev/group4/veterinaryClinic/VeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.group4.veterinaryClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

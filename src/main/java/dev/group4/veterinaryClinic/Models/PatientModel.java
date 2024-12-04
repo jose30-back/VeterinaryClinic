622c668a8bfdb337dc5ba5803923dd5890ad3979
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-12-04 16:06:16 +0100

    create model PatientModel

--- src/main/java/dev/group4/veterinaryClinic/Models/PatientModel.java
+++ src/main/java/dev/group4/veterinaryClinic/Models/PatientModel.java
@@ -1,5 +1,94 @@
 package dev.group4.veterinaryClinic.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class PatientModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int patientId;
+    private String name;
+    private int age;
+    private String race;
+    private String gender;
+    private String treatment;
+
+
+    public PatientModel(int patientId, String name, int age, String race, String gender, String treatment) {
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+        this.race = race;
+        this.gender = gender;
+        this.treatment = treatment;
+    }
+
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getRace() {
+        return race;
+    }
+
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    
+
+    
+
 
 }
--- src/main/java/dev/group4/veterinaryClinic/controllers/PatientController.java
+++ src/main/java/dev/group4/veterinaryClinic/controllers/PatientController.java
@@ -3,12 +3,27 @@ package dev.group4.veterinaryClinic.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.group4.veterinaryClinic.Services.PatientService;
+
 @RestController
 public class PatientController {
+
+
+    private PatientService service;
+
+    
+
+    public PatientController(PatientService service) {
+        this.service = service;
+    }
+
+
 
     @GetMapping("/")
     public String index(){
         return "Hello Spring";
     }
+
+    
    
 }

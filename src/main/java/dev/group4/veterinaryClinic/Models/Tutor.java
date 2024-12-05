f8bb357150a4ee6882bdf235ee8ddf4ebb52965e
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-12-05 12:38:13 +0100

    creat the tutor model

---
+++ src/main/java/dev/group4/veterinaryClinic/Models/Tutor.java
@@ -0,0 +1,115 @@
+package dev.group4.veterinaryClinic.Models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tutores")
+
+public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String FirstName;
+    private String LastName;
+    private String Phone;
+
+    
+
+    public Tutor(Long id, String firstName, String lastName, String phone, List<PatientModel> pacientes) {
+        this.id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Phone = phone;
+        this.pacientes = pacientes;
+    }
+
+    
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+
+
+
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+
+
+
+
+    public String getLastName() {
+        return LastName;
+    }
+
+
+
+
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+
+
+
+
+    public String getPhone() {
+        return Phone;
+    }
+
+
+
+
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+
+
+
+
+    public List<PatientModel> getPacientes() {
+        return pacientes;
+    }
+
+
+
+
+
+    public void setPacientes(List<PatientModel> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+
+
+
+
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL)
+    private List<PatientModel> pacientes; // Relación con Paciente
+}

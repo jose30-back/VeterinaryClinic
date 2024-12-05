9275543bc327d511307155655f6157798d785903
Anngelooo <AngeloColmenares00@gmail.com>
2024-12-05 15:51:50 +0100

    uploading AdminModel

--- src/main/java/dev/group4/veterinaryClinic/Models/AdminModel.java
+++ src/main/java/dev/group4/veterinaryClinic/Models/AdminModel.java
@@ -2,4 +2,41 @@ package dev.group4.veterinaryClinic.Models;
 
 public class AdminModel {
 
+    private int userId;
+    private String username;
+    private String email;
+
+    public AdminModel(){
+
+    }
+
+    public AdminModel(int userId, String username, String email){
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
